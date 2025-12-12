28e12883e4d35ed69382b023f41a18ed84f1658e
eungae <kyung3086@gmail.com>
2025-12-12 14:54:29 +0900

    add comment

--- hw1212/hw1/bin/hw1.dart
+++ hw1212/hw1/bin/hw1.dart
@@ -12,7 +12,11 @@ class Score {
 // Score를 상속받아 학생의 정보를 포함하는 StudentScore 클래스 구성
 class StudentScore extends Score {
   String name;
-  StudentScore(this.name, super.score);
+  StudentScore(
+    this.name,
+    super.score,
+  ); /* 여기에서 this를 쓰면 에러가 나는데 super를 쓰면 에러가 나지 않음. 
+        왜 this를 쓸 수 없는지, this를 쓰려면 어떻게 해야하는지 이해하지 못한 채 일단 super를 사용함ㅠ */
 
   @override
   void showInfo() {
